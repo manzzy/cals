d064b9741bda051d2e0d5a5dd856428c12834058
manzzy <mikegirma82@gmail.com>
2024-01-22 20:55:08 +0300

    initiate

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+
+
+document.querySelector('.calc-btn').addEventListener('click', () => {
+    let miles_ = parseFloat(document.querySelector('.miles').value)
+    document.querySelector('.fair-price').innerText = "$ " + calc_cost(miles_)
+})
+
+function calc_cost(miles) {
+    let mpg = 14.5
+    let fuel_per_gallon_cost = 3.1
+    let fuel_consumption = (miles / mpg) * fuel_per_gallon_cost
+    
+    let maintenace_cost = 100
+    let dh_componsation = 35
+    
+    let other_costs = 50
+    let variation = 0
+    if(miles > 350)
+        variation = 100
+
+    let predicted_cost = fuel_consumption + maintenace_cost + dh_componsation + other_costs + variation
+    let profit = (0.25 * predicted_cost) 
+    profit = parseInt(profit)
+
+    total = parseInt(profit + predicted_cost)
+
+
+    return total
+}
